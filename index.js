4f5210abc1a2b40d930d6d23daba18630ac766be
Ziji <Zijipia@gmail.com>
2025-02-06 03:52:52 +0700

    hm..

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
+const http = require("http");
 
-const app = express();
 const PORT = process.env.PORT || 2003;
 const WS_TARGET = process.env.WS_TARGET;
 const API_TARGET = process.env.API_TARGET;
 
-app.use(cors());
+const app = express();
+const server = http.createServer(app);
 
-// Proxy WebSocket
 app.use(
-	"/ws",
-	createProxyMiddleware({
-		target: WS_TARGET,
-		ws: true,
-		changeOrigin: true,
-		logLevel: "debug",
+	cors({
+		origin: "*",
+		methods: ["GET", "POST"],
+		credentials: true,
 	}),
 );
-
 // Proxy API requests
 app.use(
 	"/",
@@ -30,4 +27,42 @@ app.use(
 		logLevel: "debug",
 	}),
 );
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// Proxy WebSocket
+// app.use(
+// 	"/ws",
+// 	createProxyMiddleware({
+// 		target: WS_TARGET,
+// 		ws: true,
+// 		changeOrigin: true,
+// 		logLevel: "debug",
+// 	}),
+// );
+const WebSockets = require("ws");
+const wss = new WebSockets.Server({ app });
+wss.on("connection", (ws) => {
+	const wsc = new WebSocket(WS_TARGET);
+	wsc.onopen = () => {
+		console.log("WebSocket connected");
+	};
+
+	wsc.onmessage = (event) => {
+		ws.send(event);
+	};
+
+	wsc.onclose = () => {
+		console.log("WebSocket disconnected");
+	};
+
+	wsc.onerror = (error) => {
+		console.error("WebSocket error:", error);
+	};
+
+	ws.on("message", async (message) => {
+		wsc.send(message);
+	});
+
+	ws.on("close", () => {
+		console.log("[WebSocket] Client disconnected.");
+	});
+});
